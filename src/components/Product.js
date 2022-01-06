82483012c1075ea5ea731bc5060f2dd663de7847
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-06 18:35:05 -0500

    added bootstrap

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
+
 
 function Product(props) {
     return (
-       <h3>{props.product.name}</h3>
+        <Card className = 'my-3 p-3 rounded'>
+            <h3>{props.product.name}</h3>
+        </Card>
+
     )
 }
 
