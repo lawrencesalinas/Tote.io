0b6a090131bb56b89621ffc1fa7a53e6e226563e
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-06 15:00:20 -0500

    added header bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Container } from 'react-bootstrap'
+
 import Footer from './components/Footer';
 import Header from './components/Header'
 
@@ -5,6 +7,11 @@ function App() {
   return (
     <div >
       <Header/>
+      <main>
+        <Container>
+        <h1>Welcome to my Store</h1>
+        </Container>
+      </main>
       <Footer/>
 
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from "react"
+import { Container, Row, Col } from 'react-bootstrap'
 
 function Footer() {
     return (
-        <div>
-           <footer>Footer</footer> 
-        </div>
+        <footer>
+            <Container>
+                <Row>
+                    <Col className='text-center py -3'>Copyright &copy; Ecommerce</Col>
+                </Row>
+            </Container>
+        </footer> 
+        
     )
 }
 
