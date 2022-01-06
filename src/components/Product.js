85414ad4d680dd33196b675b69b548159aec5890
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-06 18:43:28 -0500

    first time pushing to remote repository

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 
-
+//bootsrap margin and padding added
 function Product(props) {
     return (
+        
         <Card className = 'my-3 p-3 rounded'>
+            <a href ={`/product/${props.product._id}`}></a>
             <h3>{props.product.name}</h3>
+            <Card.Img src = {props.product.image}/>
         </Card>
 
     )
