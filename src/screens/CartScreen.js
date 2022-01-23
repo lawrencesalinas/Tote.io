99dccb8916aa45aeb95246400458c6af7daa709e
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-23 05:50:04 -0500

    added add to cart functionality

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { Link, useParams, useLocation } from 'react-router-dom';
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
+import { second } from '../components/Message';
+import { addToCart  } from '../actions/cartActions';
+import { useDispatch, useSelector } from 'react-redux';
 
-function CartScreen() {
+function CartScreen({}) {
+    const location = useLocation()
+    const {id} = useParams()
+    const productId = id
+    // we need to get the quantity from our url, if it exist do .split which turn it to an array
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+    // console.log('qty', qty);
+  const dispacth = useDispatch()
+
+  const cart = useSelector(state => state.cart)
+  const { cartItems } = cart
+  console.log('cartItems', cartItems);
+
+  useEffect(()=> {
+        // make sure its dependent on productId
+        // dispatch update our state and add items to local storage
+      if(productId){
+          dispacth(addToCart(productId, qty))
+      }
+  }, [dispacth, productId, qty])
+
   return (
   <div>
       Cart
