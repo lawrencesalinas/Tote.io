d509accabcd4f18a0261f4618cdc33e2e8e72c19
Lawrence <lawrencesalinas@yahoo.com>
2022-02-26 14:11:00 -0500

    login loading and error handling working

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -38,6 +38,8 @@ function LoginScreen() {
   return (
     <FormContainer>
       <h1>Sign In</h1>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader/>}
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="email">
           <Form.Label>Email Address</Form.Label>
